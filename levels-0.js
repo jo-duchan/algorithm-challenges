28f108739eeafe7b9f8cc4f557152fdaf87615f4
jo-duchan <jo_duchan@icloud.com>
2024-02-11 00:38:05 +0900

    flag에 따라 다른 값 반환하기

--- levels-0.js
+++ levels-0.js
@@ -12,3 +12,14 @@ function stringRepeatOutput(str, n) {
 
 // stringRepeatOutput("string", 5);
 // stringstringstringstringstring
+
+// flag에 따라 다른 값 반환하기
+
+function incrementOrDecrement(a, b, flag) {
+  return flag ? a + b : a - b;
+}
+
+// incrementOrDecrement(-4, 7, true)
+// 3
+// incrementOrDecrement(-4, 7, false)
+// -11
