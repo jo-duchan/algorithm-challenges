ec8fe021686bee7ee06b270d1a3dc579acc36605
jo-duchan <jo_duchan@icloud.com>
2024-02-15 00:16:42 +0900

    홀짝 구분하기

--- levels-0.js
+++ levels-0.js
@@ -83,3 +83,21 @@ function changeTextVertical(str) {
 // b
 // c
 // d
+
+// 홀짝 구분하기
+
+function oddAndEven(num) {
+  let result = "";
+  if (num % 2 === 0) {
+    result = `${num} is even`;
+  } else {
+    result = `${num} is odd`;
+  }
+
+  return result;
+}
+
+// oddAndEven(100);
+// 100 is even
+// oddAndEven(1);
+// 1 is odd
