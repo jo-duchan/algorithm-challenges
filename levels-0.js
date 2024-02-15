e0042a46f207bf3c45d3e81efff7a273a1366c75
jo-duchan <jo_duchan@icloud.com>
2024-02-15 17:17:31 +0900

    문자열 겹쳐쓰기

--- levels-0.js
+++ levels-0.js
@@ -101,3 +101,12 @@ function oddAndEven(num) {
 // 100 is even
 // oddAndEven(1);
 // 1 is odd
+
+// 문지열 겹쳐쓰기
+
+function overwrite(str, over_str, n) {
+  return str.slice(0, n) + over_str + str.slice(n + over_str.length);
+}
+
+// overwrite("He11oWor1d", "lloWorl", 2);
+// HelloWorld
