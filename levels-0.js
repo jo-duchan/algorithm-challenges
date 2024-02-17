f1600b87b409b2f5c199cf515965eaac91f15b43
jo-duchan <jo_duchan@icloud.com>
2024-02-17 23:31:41 +0900

    문자열 곱하기

--- levels-0.js
+++ levels-0.js
@@ -134,3 +134,12 @@ function returnToStringListString(arr) {
 
 // returnToStringListString(["a", "b", "c"]);
 // abc
+
+// 문자열 곱하기
+
+function multiplicationOfStrings(str, n) {
+  return str.repeat(n);
+}
+
+// multiplicationOfStrings("string", 3);
+// stringstringstring
