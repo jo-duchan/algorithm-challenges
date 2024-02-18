3027dc3c39b9643468198f95a7412d45bd657d79
jo-duchan <jo_duchan@icloud.com>
2024-02-18 18:52:30 +0900

    두 수의 연산값 비교하기

--- levels-0.js
+++ levels-0.js
@@ -152,3 +152,19 @@ function returnALargerValue(a, b) {
 
 // returnALargerValue(89, 8);
 // 898
+
+// 두 수의 연산값 비교하기
+
+function comparisonOfCalculationValues(a, b) {
+  const answer1 = parseInt(`${a}${b}`);
+  const answer2 = 2 * a * b;
+
+  if (answer1 === answer2) {
+    return answer1;
+  }
+
+  return answer1 < answer2 ? answer2 : answer1;
+}
+
+// comparisonOfCalculationValues(91, 2);
+// 912
