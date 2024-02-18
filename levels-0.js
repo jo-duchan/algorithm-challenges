65fce8a7babe05a144ac8a81a15b558c1e0cb11b
jo-duchan <jo_duchan@icloud.com>
2024-02-18 23:54:23 +0900

    길이에 따른 연산

--- levels-0.js
+++ levels-0.js
@@ -211,3 +211,23 @@ function conditionString(ineq, eq, n, m) {
 // 1
 // conditionString(">", "!", 41, 78);
 // 0
+
+// 길이에 따른 연산
+
+function calculationByLength(arr) {
+  let result = 0;
+
+  if (arr.length >= 11) {
+    arr.map((num) => (result += num));
+    return result;
+  }
+
+  result = 1;
+  arr.map((num) => (result *= num));
+  return result;
+}
+
+// calculationByLength([3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1]);
+// 51
+// calculationByLength([2, 3, 4, 5]);
+// 120
