a8045adda3776def18846ffb8c7db07e5f44a2f7
jo-duchan <jo_duchan@icloud.com>
2024-02-19 13:34:48 +0900

    코드 처리하기

--- levels-0.js
+++ levels-0.js
@@ -231,3 +231,24 @@ function calculationByLength(arr) {
 // 51
 // calculationByLength([2, 3, 4, 5]);
 // 120
+
+// 코드 처리하기
+
+function processCode(code) {
+  let ret = "";
+  let mode = 0;
+
+  for (let i = 0; i < code.length; i++) {
+    if (code[i] === "1") {
+      mode = mode === 1 ? 0 : 1;
+    }
+    if (code[i] !== "1" && i % 2 === mode) {
+      ret += code[i];
+    }
+  }
+
+  return ret === "" ? "EMPTY" : ret;
+}
+
+// processCode("abc1abc1abc");
+// acbac
