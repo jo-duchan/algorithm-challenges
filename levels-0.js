c09ec559ea4d635595f60580f551309dfd12d0ac
jo-duchan <jo_duchan@icloud.com>
2024-02-19 16:50:32 +0900

    등차수열의 특정한 항만 더하기

--- levels-0.js
+++ levels-0.js
@@ -252,3 +252,26 @@ function processCode(code) {
 
 // processCode("abc1abc1abc");
 // acbac
+
+// 등차수열의 특정한 항만 더하기
+
+function addSpecificValuesOnly(a, d, included) {
+  let result = 0;
+  let ap = [];
+
+  included.map((bool, index) => {
+    ap.push(a);
+    a += d;
+
+    if (bool) {
+      result += ap[index];
+    }
+  });
+
+  return result;
+}
+
+// addSpecificValuesOnly(3, 4, [true, false, false, true, true]);
+// 37
+// addSpecificValuesOnly(7, 1, [false, false, false, true, false, false, false]);
+// 10
