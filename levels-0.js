278442f386a172fde0ac78fee8ff47bc8348624b
jo-duchan <jo_duchan@icloud.com>
2024-02-19 23:33:02 +0900

    주사위 게임 2

--- levels-0.js
+++ levels-0.js
@@ -275,3 +275,42 @@ function addSpecificValuesOnly(a, d, included) {
 // 37
 // addSpecificValuesOnly(7, 1, [false, false, false, true, false, false, false]);
 // 10
+
+// 주사위 게임 2
+
+function diceGame2(a, b, c) {
+  let arr = [a, b, c];
+
+  const comparison = (acc, cur, idx, src) => {
+    src.map((val, i) => {
+      if (idx !== i && val === cur) {
+        acc += 1;
+      }
+    });
+    return acc;
+  };
+
+  const result = arr.reduce(comparison, 0);
+
+  if (result === 0) {
+    return a + b + c;
+  }
+
+  if (0 < result && result < 6) {
+    return (a + b + c) * (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+  }
+
+  if (result === 6) {
+    return (
+      (a + b + c) *
+      (Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2)) *
+      (Math.pow(a, 3) + Math.pow(b, 3) + Math.pow(c, 3))
+    );
+  }
+}
+// diceGame2(2, 6, 1);
+// 9
+// diceGame2(5, 3, 3);
+// 473
+// diceGame2(4, 4, 4);
+// 110592
