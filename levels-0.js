3bf0bdcaacd8e9d98555964c8c10241b9a9cbaf6
jo-duchan <jo_duchan@icloud.com>
2024-02-20 15:59:48 +0900

    원소들의 곱과 합

--- levels-0.js
+++ levels-0.js
@@ -314,3 +314,20 @@ function diceGame2(a, b, c) {
 // 473
 // diceGame2(4, 4, 4);
 // 110592
+
+// 원소들의 곱과 합
+
+function multiplicationAndSumOfSquares(num_list) {
+  let sum = 0;
+  let multi = 1;
+
+  num_list.map((num) => {
+    sum += num;
+    multi *= num;
+  });
+
+  return Math.pow(sum, 2) > multi ? 1 : 0;
+}
+
+// multiplicationAndSumOfSquares([5, 7, 8, 3]);
+// 0
