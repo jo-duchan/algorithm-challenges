30fe3ece40594b3e00ab4c910df5cd5344dac919
jo-duchan <jo_duchan@icloud.com>
2024-02-21 00:35:21 +0900

    이어 붙인 수

--- levels-0.js
+++ levels-0.js
@@ -331,3 +331,21 @@ function multiplicationAndSumOfSquares(num_list) {
 
 // multiplicationAndSumOfSquares([5, 7, 8, 3]);
 // 0
+
+// 이어 붙인 수
+
+function connectNumber(num_list) {
+  const { oddStr, evenStr } = num_list.reduce(
+    ({ oddStr, evenStr }, cur) => {
+      cur % 2 === 0 ? (oddStr += cur.toString()) : (evenStr += cur.toString());
+
+      return { oddStr, evenStr };
+    },
+    { oddStr: "", evenStr: "" }
+  );
+
+  return parseInt(oddStr) + parseInt(evenStr);
+}
+
+// connectNumber([3, 4, 5, 2, 1]);
+// 393
