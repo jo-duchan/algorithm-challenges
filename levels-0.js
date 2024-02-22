36895e9f213c611d5c950948fb4a828b357c4873
jo-duchan <jo_duchan@icloud.com>
2024-02-23 00:09:37 +0900

    수 조작하기 2

--- levels-0.js
+++ levels-0.js
@@ -391,3 +391,27 @@ function numberControl(n, control) {
 
 // numberControl(0, "wsdawsdassw");
 // -1
+
+// 수 조작하기 2
+
+function numberControl2(numLog) {
+  const control = {
+    1: "w",
+    "-1": "s",
+    10: "d",
+    "-10": "a",
+  };
+
+  const result = [];
+
+  for (let i = 0; i < numLog.length; i++) {
+    if (i === 0) continue;
+
+    result.push(control[(numLog[i] - numLog[i - 1]).toString()]);
+  }
+
+  return result.join("");
+}
+
+// numberControl2([0, 1, 0, 10, 0, 1, 0, 10, 0, -1, -2, -1]);
+// wsdawsdassw
