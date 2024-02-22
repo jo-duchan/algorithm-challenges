ace4ae4518723d7d7da1fcbed359ba8f17e3e683
jo-duchan <jo_duchan@icloud.com>
2024-02-22 16:26:34 +0900

    마지막 두 원소

--- levels-0.js
+++ levels-0.js
@@ -349,3 +349,18 @@ function connectNumber(num_list) {
 
 // connectNumber([3, 4, 5, 2, 1]);
 // 393
+
+// 마지막 두 원소
+
+function lastTwoNumbers(num_list) {
+  const lastTo2ndNum = num_list[num_list.length - 2];
+  const lastNum = num_list[num_list.length - 1];
+  const addNum = lastNum > lastTo2ndNum ? lastNum - lastTo2ndNum : lastNum * 2;
+
+  return [...num_list, addNum];
+}
+
+// lastTwoNumbers([2, 1, 6]);
+// [ 2, 1, 6, 5 ]
+// lastTwoNumbers([5, 2, 1, 7, 5]);
+// [ 5, 2, 1, 7, 5, 10 ]
