0518816df079a786f96c941ad2361ffcd7a7fd29
jo-duchan <jo_duchan@icloud.com>
2024-02-25 17:09:50 +0900

    수열과 구간 쿼리 4

--- levels-0.js
+++ levels-0.js
@@ -468,3 +468,21 @@ function sequenceAndIntervalQuery2(arr, queries) {
 // [ 4 ]
 // sequenceAndIntervalQuery2([0, 1, 2, 4, 3], [[0, 4, 2], [0, 3, 2], [0, 2, 2]]);
 // [3, 4, -1]
+
+// 수열과 구간 쿼리 4
+
+function sequenceAndIntervalQuery4(arr, queries) {
+  queries.map(([s, e, k]) => {
+    arr = arr.map((n, i) => {
+      if (s <= i && i <= e && i % k === 0) {
+        return ++n;
+      }
+      return n;
+    });
+  });
+
+  return arr;
+}
+
+// sequenceAndIntervalQuery4([0, 1, 2, 4, 3],[[0, 4, 1],[0, 3, 2],[0, 3, 3],]);
+// [ 3, 2, 4, 6, 4 ]
