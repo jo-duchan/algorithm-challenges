098eae8479ae5a615eaa36b17b8a043b9d4db80b
jo-duchan <jo_duchan@icloud.com>
2024-02-27 17:21:55 +0900

    배열 만들기 2

--- levels-0.js
+++ levels-0.js
@@ -495,3 +495,20 @@ function convertString(n) {
 
 // convertString(123);
 // 123
+
+// 배열 만들기 2
+
+function createArrays(l, r) {
+  const result = [];
+
+  for (let i = l; i <= r; i++) {
+    if (/^[05]+$/.test(i)) {
+      result.push(i);
+    }
+  }
+
+  return result.length <= 0 ? [-1] : result;
+}
+
+// createArrays(5, 5005);
+// [5, 50, 55, 500, 505, 550, 555]
