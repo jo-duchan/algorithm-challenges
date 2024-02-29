c16baa44ee23ecdfc990e7f05bc4a6ab3b2aac8b
jo-duchan <jo_duchan@icloud.com>
2024-03-01 01:37:39 +0900

    배열 만들기 4

--- levels-0.js
+++ levels-0.js
@@ -545,3 +545,31 @@ function collatzSequence(n, arr = []) {
 
 collatzSequence(10);
 // [10, 5, 16, 8, 4, 2, 1]
+
+// 배열 만들기 4
+
+function createArrays4(arr) {
+  let i = 0;
+  const stk = [];
+
+  while (i < arr.length) {
+    if (stk.length === 0) {
+      stk.push(arr[i]);
+      i++;
+    }
+
+    if (stk[stk.length - 1] < arr[i]) {
+      stk.push(arr[i]);
+      i++;
+    }
+
+    if (stk[stk.length - 1] >= arr[i]) {
+      stk.pop();
+    }
+  }
+
+  return stk;
+}
+
+// createArrays4([1, 4, 2, 5, 3]);
+// [ 1, 2, 3 ]
