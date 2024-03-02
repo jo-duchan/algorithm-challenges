a84eccdda0ceb605284e98c4406093677119acd6
jo-duchan <jo_duchan@icloud.com>
2024-03-02 16:38:29 +0900

    간단한 논리 연

--- levels-0.js
+++ levels-0.js
@@ -573,3 +573,14 @@ function createArrays4(arr) {
 
 // createArrays4([1, 4, 2, 5, 3]);
 // [ 1, 2, 3 ]
+
+// 간단한 논리 연산
+
+function simpleLogicalOperation(x1, x2, x3, x4) {
+  return (x1 || x2) && (x3 || x4);
+}
+
+// simpleLogicalOperation(false, true, true, true);
+// true
+// simpleLogicalOperation(true, false, false, false);
+// false
