f7fd9d104de25d4078ff9470eca5694a9ed6df9a
jo-duchan <jo_duchan@icloud.com>
2024-03-03 20:36:20 +0900

    주사위 게임3

--- levels-0.js
+++ levels-0.js
@@ -584,3 +584,47 @@ function simpleLogicalOperation(x1, x2, x3, x4) {
 // true
 // simpleLogicalOperation(true, false, false, false);
 // false
+
+// 주사위 게임 3
+
+function count(arr) {
+  const counter = new Map();
+  for (const data of arr) {
+    counter.set(data, (counter.get(data) || 0) + 1);
+  }
+
+  const sortedByCnt = [...counter.keys()].sort(
+    (a, b) => counter.get(b) - counter.get(a)
+  );
+  const maxCnt = Math.max(...counter.values());
+
+  return [sortedByCnt, maxCnt];
+}
+
+function diceGame3(a, b, c, d) {
+  const [arr, maxCnt] = count([a, b, c, d]);
+  const [p, q, r, s] = arr;
+
+  if (arr.length === 1) {
+    return p * 1111;
+  }
+
+  if (arr.length === 2) {
+    return maxCnt === 2 ? (p + q) * Math.abs(p - q) : Math.pow(10 * p + q, 2);
+  }
+
+  if (arr.length === 3) {
+    return q * r;
+  }
+
+  return Math.min(p, q, r, s);
+}
+
+// diceGame3(2, 2, 2, 2);
+// 2222
+// diceGame3(4, 1, 4, 4);
+// 1681
+// diceGame3(6, 3, 3, 6);
+// 27
+// diceGame3(2, 5, 2, 6);
+// 30
