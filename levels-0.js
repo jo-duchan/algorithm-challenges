14aeeda9a591828300dc79e4e3c2c6c8035ce729
jo-duchan <jo_duchan@icloud.com>
2024-03-06 10:40:08 +0900

    문자열 여러 번 뒤집기

--- levels-0.js
+++ levels-0.js
@@ -657,3 +657,25 @@ function divideBy9(number) {
 // 	6
 // divideBy9("78720646226947352489");
 // 2
+
+// 문자열 여러 번 뒤집기
+
+function flipTheString(string, queries) {
+  const result = queries.reduce((string, [s, e]) => {
+    let arr = [...string];
+    const newString = arr.slice(s, e + 1).reverse();
+    arr.splice(s, newString.length, ...newString);
+
+    return arr.join("");
+  }, string);
+
+  return result;
+}
+
+// flipTheString("rermgorpsam", [
+//   [2, 3],
+//   [0, 7],
+//   [5, 9],
+//   [6, 10],
+// ]);
+// programmers
