8512cfc4ac83ce940e331e9a287149b53d3a2e08
jo-duchan <jo_duchan@icloud.com>
2024-03-09 00:20:09 +0900

    문자열의 뒤의 n글자

--- levels-0.js
+++ levels-0.js
@@ -717,3 +717,12 @@ function toAttachPieceOfString(str_arr, parts) {
 //   ]
 // );
 // programmers
+
+// 문자열의 뒤의 n글자
+
+function nthFromTheBack(string, n) {
+  return string.slice(-n);
+}
+
+// nthFromTheBack("He110W0r1d", 5);
+// W0r1d
