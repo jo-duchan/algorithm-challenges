6f2c1cf4ad3890845351c9b3c8a79a64a8b7efae
jo-duchan <jo_duchan@icloud.com>
2024-03-10 14:21:41 +0900

    접미사 배열

--- levels-0.js
+++ levels-0.js
@@ -726,3 +726,17 @@ function nthFromTheBack(string, n) {
 
 // nthFromTheBack("He110W0r1d", 5);
 // W0r1d
+
+// 접미사 배열
+
+function suffixArray(string) {
+  const result = [];
+  for (let i = 0; i < string.length; i++) {
+    result.push(string.slice(-i));
+  }
+
+  return result.sort();
+}
+
+// suffixArray("banana");
+// [ 'a', 'ana', 'anana', 'banana', 'na', 'nana' ]
