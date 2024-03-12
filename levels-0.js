470507165a5e90431bf9c7d92ad6156981f38d55
jo-duchan <jo_duchan@icloud.com>
2024-03-12 10:29:01 +0900

    문자열 뒤집기

--- levels-0.js
+++ levels-0.js
@@ -757,3 +757,16 @@ function prefixCheck(string, prefix) {
 }
 
 prefixCheck("banana", "nan");
+
+// 문자열 뒤집기
+
+function flipTheString2(string, s, e) {
+  const arry = Array.from(string);
+  const flipString = arry.slice(s, e + 1).reverse();
+  arry.splice(s, flipString.length, ...flipString);
+
+  return arry.join("");
+}
+
+flipTheString2("Progra21Sremm3", 6, 12);
+// ProgrammerS123
