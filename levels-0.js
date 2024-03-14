37211a84c7bfd351462baf0c642da157149a26fb
jo-duchan <jo_duchan@icloud.com>
2024-03-14 15:12:06 +0900

    세로 읽기

--- levels-0.js
+++ levels-0.js
@@ -783,3 +783,18 @@ function qrcode(q, r, code) {
 
 qrcode(3, 1, "qjnwezgrpirldywt");
 // jerry
+
+// 세로 읽기
+
+function verticalRead(string, m, c) {
+  const result = Array.from(string)
+    .filter((_, i) => (i % m) + 1 === c)
+    .join("");
+
+  return result;
+}
+
+// verticalRead("ihrhbakrfpndopljhygc", 4, 2);
+// happy
+// verticalRead("programmers", 1, 1);
+// programmers
