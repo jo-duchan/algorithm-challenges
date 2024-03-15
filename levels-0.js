36daeb5e93f8f4b5c9e0dc7dda006a782c854d4c
jo-duchan <jo_duchan@icloud.com>
2024-03-15 13:26:38 +0900

    문자 개수 세기

--- levels-0.js
+++ levels-0.js
@@ -798,3 +798,25 @@ function verticalRead(string, m, c) {
 // happy
 // verticalRead("programmers", 1, 1);
 // programmers
+
+// 문자 개수 세기
+
+function theNumberOfLetters(string) {
+  const alphabet = Array(52).fill(0);
+
+  Array.from(string).forEach((x, idx) => {
+    const charCode = string.charCodeAt(idx);
+
+    if (charCode >= 65 && charCode <= 90) {
+      alphabet[charCode - 65] += 1;
+    }
+
+    if (charCode >= 97 && charCode <= 122) {
+      alphabet[charCode - 97 + 26] += 1;
+    }
+  });
+
+  return alphabet;
+}
+
+theNumberOfLetters("Programmers");
