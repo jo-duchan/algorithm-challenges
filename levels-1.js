4864363bfaa2f4e04eccb4906642a72e325bab79
jo-duchan <jo_duchan@icloud.com>
2024-03-17 22:49:20 +0900

    가장 많이 받은 선물(2024 KAKAO WINTER INTERNSHIP)

--- levels-1.js
+++ levels-1.js
@@ -21,8 +21,58 @@ function racingGame(players, callings) {
   return players;
 }
 
-racingGame(
-  ["mumu", "soe", "poe", "kai", "mine"],
-  ["kai", "kai", "mine", "mine"]
-);
+// racingGame(
+//   ["mumu", "soe", "poe", "kai", "mine"],
+//   ["kai", "kai", "mine", "mine"]
+// );
 // [ 'mumu', 'kai', 'mine', 'soe', 'poe' ]
+
+// 가장 많이 받은 선물(2024 KAKAO WINTER INTERNSHIP)
+
+function theMostGiftedGift(friends, gifts) {
+  let answer = 0;
+
+  const giftTable = Object.fromEntries(
+    friends.map((friend) => [
+      friend,
+      Object.fromEntries(friends.map((friend) => [friend, 0])),
+    ])
+  );
+
+  const giftParams = Object.fromEntries(friends.map((friend) => [friend, 0]));
+
+  gifts.forEach((gift) => {
+    const [give, take] = gift.split(" ");
+    giftParams[give]++;
+    giftParams[take]--;
+    giftTable[give][take]++;
+  });
+
+  friends.forEach((me) => {
+    let temp = 0;
+    friends.forEach((firend) => {
+      const giveNum = giftTable[me][firend];
+      const takeNum = giftTable[firend][me];
+      if (giveNum > takeNum) temp++;
+      if (giveNum === takeNum && giftParams[me] > giftParams[firend]) temp++;
+    });
+    if (temp > answer) answer = temp;
+  });
+
+  return answer;
+}
+
+// theMostGiftedGift(
+//   ["muzi", "ryan", "frodo", "neo"],
+//   [
+//     "muzi frodo",
+//     "muzi frodo",
+//     "ryan muzi",
+//     "ryan muzi",
+//     "ryan muzi",
+//     "frodo muzi",
+//     "frodo ryan",
+//     "neo muzi",
+//   ]
+// );
+// 2
