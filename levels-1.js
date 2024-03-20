2a3da30ec52c7993b28b1e109a0ac583b511f652
jo-duchan <jo_duchan@icloud.com>
2024-03-20 10:25:45 +0900

    추억 점수

--- levels-1.js
+++ levels-1.js
@@ -114,3 +114,32 @@ function bandageGame(bandage, health, attacks) {
 // -1
 // bandageGame([5, 1, 5], 30, [[2, 10], [9, 15], [10, 5], [11, 5]])
 // 5
+
+// 추억 점수
+
+function memoriesScore(name, yearning, photo) {
+  const matchingData = name.reduce((acc, cur, idx) => {
+    return [...acc, [cur, yearning[idx]]];
+  }, []);
+  const scoreMap = new Map(matchingData);
+
+  const result = photo.reduce((acc, cur) => {
+    let score = 0;
+    cur.forEach((name) => (score += scoreMap.get(name) ?? 0));
+
+    return [...acc, score];
+  }, []);
+
+  return result;
+}
+
+// memoriesScore(
+//   ["may", "kein", "kain", "radi"],
+//   [5, 10, 1, 3],
+//   [
+//     ["may", "kein", "kain", "radi"],
+//     ["may", "kein", "brin", "deny"],
+//     ["kon", "kain", "may", "coni"],
+//   ]
+// );
+// [ 19, 15, 6 ]
