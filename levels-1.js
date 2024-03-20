2a4fca79acdbb69c58ac1200fc3bdba3080d8aa0
jo-duchan <jo_duchan@icloud.com>
2024-03-20 15:31:01 +0900

    크기가 작은 부분문자열

--- levels-1.js
+++ levels-1.js
@@ -143,3 +143,20 @@ function memoriesScore(name, yearning, photo) {
 //   ]
 // );
 // [ 19, 15, 6 ]
+
+// 크기가 작은 부분문자열
+
+function substringComparison(t, p) {
+  let count = 0;
+
+  for (let i = 0; i <= t.length - p.length; i++) {
+    const num = t.slice(i, p.length + i);
+
+    if (parseInt(p) >= parseInt(num)) count++;
+  }
+
+  return count;
+}
+
+// substringComparison("500220839878", "7");
+// 8
