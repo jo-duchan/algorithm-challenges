3056770b087cb15bd673f5e68cd30b338c0a8a91
jo-duchan <jo_duchan@icloud.com>
2024-03-22 11:14:41 +0900

    가장 가까운 같은 글자

--- levels-1.js
+++ levels-1.js
@@ -191,3 +191,20 @@ function bunchOfCards(cards1, cards2, goal) {
 //   ["i", "want", "to", "drink", "water"]
 // );
 // Yes
+
+// 가장 가까운 같은 글자
+function theNearestLetter(s) {
+  const hash = {};
+
+  const result = [...s].map((v, i) => {
+    let afterIdxArr = hash[v] === undefined ? -1 : i - hash[v];
+    hash[v] = i;
+
+    return afterIdxArr;
+  });
+
+  return result;
+}
+
+// theNearestLetter("banana");
+// [ -1, -1, -1, 2, 2, 2 ]
