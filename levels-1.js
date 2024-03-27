a8128cf1a130d49bb817afdfcb9a49660bcfc781
jo-duchan <jo_duchan@icloud.com>
2024-03-27 22:28:58 +0900

    푸드 파이트 대회

--- levels-1.js
+++ levels-1.js
@@ -232,3 +232,21 @@ function paintOver(n, m, section) {
 // 1
 // paintOver(6, 1, [1, 3, 5]);
 // 3
+
+// 푸드 파이트 대회
+
+function foodFightChallenge(food) {
+  const list = food.reduce((acc, food, idx) => {
+    if (idx !== 0) {
+      const num = Math.floor(food / 2);
+      return [...acc, ...Array(num).fill(idx)];
+    }
+
+    return acc;
+  }, []);
+
+  return [...list, 0, ...list.reverse()].join("");
+}
+
+// foodFightChallenge([1, 3, 4, 6]);
+// "1223330333221"
