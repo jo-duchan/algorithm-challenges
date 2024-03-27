7ce28a8611d759eaf59146cf90dee124774c584b
jo-duchan <jo_duchan@icloud.com>
2024-03-27 10:11:27 +0900

    배열 만들기 3

--- levels-0.js
+++ levels-0.js
@@ -917,3 +917,22 @@ function firstNegativeNumber(list) {
 
 // firstNegativeNumber([12, 4, 15, 46, 38, -2, 15]);
 // 5
+
+// 배열 만들기 3
+
+function createArray3(arr, intervals) {
+  const result = intervals.reduce((acc, [start, end]) => {
+    return [...acc, ...arr.slice(start, end + 1)];
+  }, []);
+
+  return result;
+}
+
+// createArray3(
+//   [1, 2, 3, 4, 5],
+//   [
+//     [1, 3],
+//     [0, 4],
+//   ]
+// );
+// [2, 3, 4, 1, 2, 3, 4, 5];
