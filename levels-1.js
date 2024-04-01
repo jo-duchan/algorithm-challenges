f4bfedefcd56b8832b5c68e9e4ad6eb51d03747b
jo-duchan <jo_duchan@icloud.com>
2024-04-01 15:12:26 +0900

    명예의 전당

--- levels-1.js
+++ levels-1.js
@@ -339,3 +339,29 @@ function alwaysOneLeft(n) {
 
 // alwaysOneLeft(10);
 // 3
+
+// 명예의 전당
+
+function topRankList(k, score) {
+  const result = [];
+
+  score.reduce((list, cur) => {
+    if (list.length === k && list[0] < cur) {
+      list[0] = cur;
+    }
+
+    if (list.length < k) {
+      list.push(cur);
+    }
+
+    list.sort((a, b) => a - b);
+    result.push(list[0]);
+
+    return list;
+  }, []);
+
+  return result;
+}
+
+// topRankList(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]);
+// [0, 0, 0, 0, 20, 40, 70, 70, 150, 300];
