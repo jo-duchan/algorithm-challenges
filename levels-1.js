2cab642ddf836b54eb41d0cd42efd8e6753a9dd7
jo-duchan <jo_duchan@icloud.com>
2024-04-02 10:45:03 +0900

    삼총사

--- levels-1.js
+++ levels-1.js
@@ -365,3 +365,26 @@ function topRankList(k, score) {
 
 // topRankList(4, [0, 300, 40, 300, 20, 70, 150, 50, 500, 1000]);
 // [0, 0, 0, 0, 20, 40, 70, 70, 150, 300];
+
+// 삼총사
+
+function threesome(num) {
+  let result = 0;
+
+  const recursive = (cur, init) => {
+    if (cur.length === 3) {
+      result += cur.reduce((acc, cur) => acc + cur, 0) === 0 ? 1 : 0;
+    }
+
+    for (let i = init; i < num.length; i++) {
+      recursive([...cur, num[i]], i + 1);
+    }
+  };
+
+  recursive([], 0);
+
+  return result;
+}
+
+// threesome([-2, 3, 0, 2, -5]);
+// 2
