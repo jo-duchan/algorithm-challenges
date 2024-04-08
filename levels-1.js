6e650fca17ee3d81f55cd039c12ae4259773cd44
jo-duchan <jo_duchan@icloud.com>
2024-04-08 17:48:13 +0900

    최소 직사각형

--- levels-1.js
+++ levels-1.js
@@ -506,3 +506,23 @@ function addTwoNumbers(numbers) {
 
 // addTwoNumbers([2, 1, 3, 4, 1]);
 // [ 2, 3, 4, 5, 6, 7 ]
+
+// 최소 직사각형
+
+function minimumRectangle(sizes) {
+  let maxSize = [0, 0];
+  const rotated = sizes.map(([w, h]) => (w < h ? [h, w] : [w, h]));
+
+  rotated.forEach(([w, h]) => {
+    if (w > maxSize[0]) maxSize[0] = w;
+    if (h > maxSize[1]) maxSize[1] = h;
+  });
+
+  return maxSize[0] * maxSize[1];
+}
+
+// minimumRectangle([
+//   [20, 10],
+//   [12, 18],
+// ]);
+// 240
