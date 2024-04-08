78c96f1b3ad7322dbe14afef1fd6576bf2230fea
jo-duchan <jo_duchan@icloud.com>
2024-04-08 19:11:52 +0900

    대충 만든 자판

--- levels-1.js
+++ levels-1.js
@@ -526,3 +526,36 @@ function minimumRectangle(sizes) {
 //   [12, 18],
 // ]);
 // 240
+
+// 대충 만든 자판
+
+function typing(keymap, targets) {
+  let newKeys = new Map();
+
+  keymap.forEach((keys) => {
+    [...keys].forEach((key, idx) => {
+      const count = idx + 1;
+      if (newKeys.has(key)) {
+        newKeys.get(key) > count ? newKeys.set(key, count) : null;
+      } else {
+        newKeys.set(key, count);
+      }
+    });
+  });
+
+  const result = targets.map((str) => {
+    let sum = 0;
+
+    for (let x of str) {
+      if (!newKeys.has(x)) return -1;
+      sum += +newKeys.get(x);
+    }
+
+    return sum;
+  });
+
+  return result;
+}
+
+// typing(["ABCE"], ["ABDE"]);
+// -1
