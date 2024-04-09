f85b20e07bc6fcb97107d2b5ddb34006a0259a43
jo-duchan <jo_duchan@icloud.com>
2024-04-09 13:31:46 +0900

    둘만의 암호

--- levels-1.js
+++ levels-1.js
@@ -576,3 +576,31 @@ function removeMinNumber(arr) {
 // [4, 3, 2]
 // removeMinNumber([5]);
 // [ -1 ]
+
+// 둘만의 암호
+
+function secretCode(s, skip, index) {
+  const skipMap = new Map();
+
+  [...skip].forEach((x) => skipMap.set(x.charCodeAt(), x));
+  const getIndex = (num) => ((num - 97) % 26) + 97;
+
+  const result = [...s].reduce((acc, cur) => {
+    const curASCII = cur.charCodeAt();
+    let count = index;
+
+    for (let i = 1; i <= count; i++) {
+      if (skipMap.get(getIndex(i + curASCII))) count++;
+    }
+
+    return (acc += String.fromCharCode(getIndex(curASCII + count)));
+  }, "");
+
+  console.log(result);
+  return result;
+}
+
+// secretCode("aukks", "wbqd", 5);
+// happy
+// secretCode("zzzzz", "abcdefghijklmnopqrstuvwxy", 6);
+// zzzzz
