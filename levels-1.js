e8247442d5fcf2b511b648faa37da29d838d2153
jo-duchan <jo_duchan@icloud.com>
2024-04-09 10:53:08 +0900

    제일 작은 수 제거

--- levels-1.js
+++ levels-1.js
@@ -559,3 +559,20 @@ function typing(keymap, targets) {
 
 // typing(["ABCE"], ["ABDE"]);
 // -1
+
+// 제일 작은 수 제거
+
+function removeMinNumber(arr) {
+  const smallest = Math.min(...arr);
+  const result = arr.filter((num) => num > smallest);
+
+  if (result.length === 0) {
+    return [-1];
+  }
+
+  return result;
+}
+// removeMinNumber([4, 3, 2, 1]);
+// [4, 3, 2]
+// removeMinNumber([5]);
+// [ -1 ]
