8a41d3651dcb226b51c6acba945b8cf9ae0891ed
jo-duchan <jo_duchan@icloud.com>
2024-04-10 14:52:26 +0900

    약수의 개수와 덧셈

--- levels-1.js
+++ levels-1.js
@@ -604,3 +604,30 @@ function secretCode(s, skip, index) {
 // happy
 // secretCode("zzzzz", "abcdefghijklmnopqrstuvwxy", 6);
 // zzzzz
+
+function numberAndSum(left, right) {
+  let result = 0;
+
+  const getDivisor = (num) => {
+    let count = 0;
+    for (let i = 1; i <= num; i++) {
+      if (num % i === 0) count++;
+    }
+    return count;
+  };
+
+  for (let i = left; i <= right; i++) {
+    // 제곱근이 정수면 약수의 갯수가 홀수
+    // Number.isInteger(Math.sqrt(i))
+    if (getDivisor(i) % 2 === 0) {
+      result += i;
+    } else {
+      result -= i;
+    }
+  }
+
+  return result;
+}
+
+// numberAndSum(13, 17);
+// 43
