9987d894fe1e0dcc58a3cf16d9f11ee83b5f5b81
duchan_jo <https://gitlab.com/duchan_jo>
2024-04-12 16:45:20 +0900

    K번째 수

--- levels-1.js
+++ levels-1.js
@@ -687,4 +687,25 @@ function recommendNewID(new_id) {
 // recommendNewID("z-+.^.");
 // z--
 
-//
+// K번째 수
+
+function theKthNumber(array, commands) {
+  const result = commands.reduce((acc, cur) => {
+    const [i, j, k] = cur;
+    const arr = array.slice(i - 1, j).sort((a, b) => a - b);
+
+    return [...acc, arr[k - 1]];
+  }, []);
+
+  return result;
+}
+
+// theKthNumber(
+//   [1, 5, 2, 6, 3, 7, 4],
+//   [
+//     [2, 5, 3],
+//     [4, 4, 1],
+//     [1, 7, 3],
+//   ]
+// );
+// [ 5, 6, 3 ]
