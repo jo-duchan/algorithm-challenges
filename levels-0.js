bc2d3f087af79580456ad37516bd1bfbf09d18a2
jo-duchan <jo_duchan@icloud.com>
2024-04-15 10:38:36 +0900

    n개 간격의 원소들

--- levels-0.js
+++ levels-0.js
@@ -985,3 +985,13 @@ function orderChange(list, n) {
 
 // orderChange([2, 1, 6], 1);
 // [ 1, 6, 2 ]
+
+// n개 간격의 원소들
+
+function nGapNumbers(list, n) {
+  const result = list.filter((_, i) => !(i % n));
+
+  return result;
+}
+// nGapNumbers([4, 2, 6, 1, 7, 6], 2);
+// [ 4, 6, 7 ]
