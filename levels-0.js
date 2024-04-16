1a569d708a2fd94ae59efd44c962b68523c4a770
jo-duchan <jo_duchan@icloud.com>
2024-04-16 10:44:55 +0900

    왼쪽 오른쪽

--- levels-0.js
+++ levels-0.js
@@ -995,3 +995,22 @@ function nGapNumbers(list, n) {
 }
 // nGapNumbers([4, 2, 6, 1, 7, 6], 2);
 // [ 4, 6, 7 ]
+
+// 왼쪽 오른쪽
+
+function leftOrRight(list) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === "l") {
+      return list.slice(0, i);
+    }
+
+    if (list[i] === "r") {
+      return list.slice(i + 1);
+    }
+  }
+
+  return [];
+}
+
+// leftOrRight(["u", "u", "l", "r"]);
+// [ 'u', 'u' ]
