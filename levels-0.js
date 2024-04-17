ea3db5526647e5daf4d647af23745e94e5184c66
jo-duchan <jo_duchan@icloud.com>
2024-04-17 11:06:22 +0900

    홀수 vs 짝수

--- levels-0.js
+++ levels-0.js
@@ -1014,3 +1014,24 @@ function leftOrRight(list) {
 
 // leftOrRight(["u", "u", "l", "r"]);
 // [ 'u', 'u' ]
+
+// 홀수 vs 짝수
+
+function oddVersusEven(list) {
+  const result = list.reduce(
+    ([even, odd], cur, idx) => {
+      if (idx % 2 === 0) {
+        even += cur;
+      } else {
+        odd += cur;
+      }
+      return [even, odd];
+    },
+    [0, 0]
+  );
+
+  return Math.max(...result);
+}
+
+// oddVersusEven([4, 2, 6, 1, 7, 6]);
+// 17
