6a2d5c8d905b7b07e990c3a0393429bc9947c8b4
jo-duchan <jo_duchan@icloud.com>
2024-04-18 15:15:44 +0900

    5명씩

--- levels-0.js
+++ levels-0.js
@@ -1035,3 +1035,18 @@ function oddVersusEven(list) {
 
 // oddVersusEven([4, 2, 6, 1, 7, 6]);
 // 17
+
+// 5명씩
+
+function fivePeopleEach(names) {
+  const result = [];
+
+  for (let i = 0; i < names.length; i += 5) {
+    result.push(names[i]);
+  }
+
+  return result;
+}
+
+// fivePeopleEach(["nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"]);
+// ["nami", "vex"];
