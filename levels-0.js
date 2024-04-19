484d6c084bed630e43bbdb92c6693e827c1dd7a6
jo-duchan <jo_duchan@icloud.com>
2024-04-19 10:20:09 +0900

    n보다 커질 때까지 더하기

--- levels-0.js
+++ levels-0.js
@@ -1062,3 +1062,16 @@ function todoList(list, finished) {
 //   ["problemsolving", "practiceguitar", "swim", "studygraph"],
 //   [true, false, true, false]
 // );
+
+// n보다 커질 때까지 더하기
+
+function createLargerNumber(numbers, n) {
+  const result = numbers.reduce(
+    (acc, cur) => (acc > n ? acc : (acc += cur)),
+    0
+  );
+  return result;
+}
+
+// createLargerNumber([34, 5, 71, 29, 100, 34], 123);
+// 139
