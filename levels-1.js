18667e02a71b3e4b61af36f3c04a353d04b362c7
jo-duchan <jo_duchan@icloud.com>
2024-04-22 15:18:23 +0900

    [1차]비밀지도 2023 KAKAO BLIND RECRUITMENT

--- levels-1.js
+++ levels-1.js
@@ -769,3 +769,22 @@ function rectangleMaker(a, b) {
 // *****
 // *****
 // *****
+
+// [1차]비밀지도 2023 KAKAO BLIND RECRUITMENT
+
+function mapDecoder(n, arr1, arr2) {
+  const result = [];
+  for (let i = 0; i < n; i++) {
+    const sumMap = (arr1[i] | arr2[i])
+      .toString(2)
+      .padStart(n, "0")
+      .replaceAll("1", "#")
+      .replaceAll("0", " ");
+    result.push(sumMap);
+  }
+
+  return result;
+}
+
+// mapDecoder(5, [9, 20, 28, 18, 11], [30, 1, 21, 17, 28]);
+// [ '#####', '# # #', '### #', '#  ##', '#####' ]
