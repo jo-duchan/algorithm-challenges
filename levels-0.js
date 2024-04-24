eeb30f55114cf8571fb54bfeb351f75f3bde0429
jo-duchan <jo_duchan@icloud.com>
2024-04-24 10:23:42 +0900

    조건에 맞게 수열 변환하기 1

--- levels-0.js
+++ levels-0.js
@@ -1101,3 +1101,22 @@ function sectionQueries1(arr, queries) {
 //   ]
 // );
 // [ 1, 3, 4, 4, 4 ]
+
+// 조건에 맞게 수열 변환하기 1
+
+function convertSequenceOfNumber(arr) {
+  const result = arr.map((x) => {
+    if (50 <= x && x % 2 === 0) {
+      return x / 2;
+    }
+
+    if (50 > x && x % 2 !== 0) {
+      return x * 2;
+    }
+    return x;
+  });
+  return result;
+}
+
+// convertSequenceOfNumber([1, 2, 3, 100, 99, 98]);
+// [ 2, 2, 6, 50, 99, 49 ]
