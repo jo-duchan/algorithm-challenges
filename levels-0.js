7e9c49b6a69bbaeb1ad2746d8981fe696b4d93cc
jo-duchan <jo_duchan@icloud.com>
2024-04-25 13:18:17 +0900

    조건에 맞게 수열 변환하기 2

--- levels-0.js
+++ levels-0.js
@@ -1120,3 +1120,41 @@ function convertSequenceOfNumber(arr) {
 
 // convertSequenceOfNumber([1, 2, 3, 100, 99, 98]);
 // [ 2, 2, 6, 50, 99, 49 ]
+
+// 조건에 맞게 수열 변환하기 2
+
+function convertSequenceOfNumber2(arr) {
+  let count = 0;
+  let repeat = true;
+  let oldArr = arr;
+  let newArr;
+
+  const calculate = (x) => {
+    if (50 <= x && x % 2 === 0) {
+      return x / 2;
+    }
+
+    if (50 > x && x % 2 !== 0) {
+      return x * 2 + 1;
+    }
+    return x;
+  };
+
+  while (repeat) {
+    newArr = oldArr.map((x) => calculate(x));
+
+    if (newArr.toString() === oldArr.toString()) {
+      repeat = false;
+    } else {
+      oldArr = newArr;
+      count++;
+    }
+  }
+
+  return count;
+}
+
+// convertSequenceOfNumber2([1, 2, 3, 100, 99, 98]);
+// 5
+// convertSequenceOfNumber2([2]);
+// 0
