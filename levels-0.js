74fbb53f9ac8839667958b8b89ee747baa6864d3
jo-duchan <jo_duchan@icloud.com>
2024-04-26 10:21:36 +0900

    1로 만들기

--- levels-0.js
+++ levels-0.js
@@ -1158,3 +1158,27 @@ function convertSequenceOfNumber2(arr) {
 // 5
 // convertSequenceOfNumber2([2]);
 // 0
+
+// 1로 만들기
+
+function makeIt1(list) {
+  let count = 0;
+
+  const calculate = (num) => {
+    while (num > 1) {
+      count++;
+      if (num % 2 === 0) {
+        num = num / 2;
+      } else {
+        num = (num - 1) / 2;
+      }
+    }
+  };
+
+  list.forEach((num) => calculate(num));
+
+  return count;
+}
+
+// makeIt1([12, 4, 15, 1, 14]);
+// 11
