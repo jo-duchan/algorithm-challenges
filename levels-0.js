78658340ca7475193d5ef54e135f94b84cb52a89
jo-duchan <jo_duchan@icloud.com>
2024-04-28 22:18:04 +0900

    원하는 문자열 찾기

--- levels-0.js
+++ levels-0.js
@@ -1182,3 +1182,13 @@ function makeIt1(list) {
 
 // makeIt1([12, 4, 15, 1, 14]);
 // 11
+
+// 원하는 문자열 찾기
+
+function searchString(str, pat) {
+  const result = str.toLowerCase().includes(pat.toLowerCase());
+  return result ? 1 : 0;
+}
+
+// searchString("AbCdEfG", "aBc");
+// 1
