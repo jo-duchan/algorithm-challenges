9dbefcaed7c6a196e75e9ceb05c85b742d162b44
jo-duchan <jo_duchan@icloud.com>
2024-04-29 10:24:44 +0900

    배열에서 문자열 대소문자 변환하기

--- levels-0.js
+++ levels-0.js
@@ -1192,3 +1192,16 @@ function searchString(str, pat) {
 
 // searchString("AbCdEfG", "aBc");
 // 1
+
+// 배열에서 문자열 대소문자 변환하기
+
+function caseConversion(arr) {
+  const result = arr.map((str, idx) =>
+    !(idx % 2) ? str.toLowerCase() : str.toUpperCase()
+  );
+
+  return result;
+}
+
+// caseConversion(["AAA", "BBB", "CCC", "DDD"]);
+// [ 'aaa', 'BBB', 'ccc', 'DDD' ]
