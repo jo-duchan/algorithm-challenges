9a2814ca1c0f6fe4455aa805b8e09db54c75c2db
jo-duchan <jo_duchan@icloud.com>
2024-05-14 10:27:23 +0900

    문자열을 정수로 바꾸기

---
+++ levels-1/046-convert-string-to-number.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/12925
+// 문자열을 정수로 바꾸기
+
+function convertStringToNumber(s) {
+  return parseInt(s);
+}
+
+convertStringToNumber("+1234");
+// 1234
