3b06ab01d49c60f12fab2c4a57e252d97c87bee0
jo-duchan <jo_duchan@icloud.com>
2024-09-12 23:54:08 +0900

    배열 두 배 만들기

---
+++ levels-0/102-배열-두-배-만들기.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809
+
+function solution(numbers) {
+  const answer = numbers.map((num) => num * 2);
+  return answer;
+}
+
+// solution([1, 2, 3, 4, 5]);
+// [2, 4, 6, 8, 10]
