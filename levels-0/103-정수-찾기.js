daa5c93a6e7488226913bcf780b50341b6f49e40
jo-duchan <jo_duchan@icloud.com>
2024-09-13 22:49:49 +0900

    정수 찾기

---
+++ levels-0/103-정수-찾기.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181840
+
+function solution(num_list, n) {
+  return num_list.includes(n) ? 1 : 0;
+}
+
+// solution([1, 2, 3, 4, 5], 3)
+// 1
