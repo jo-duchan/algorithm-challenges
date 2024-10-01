b5e338bb6787feaf584f9a5fb640cbfdd25f924d
jo-duchan <jo_duchan@icloud.com>
2024-10-01 12:24:33 +0900

    두 수의 나눗셈

---
+++ levels-1/106-두-수의-나눗셈.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+
+function solution(num1, num2) {
+  const answer = parseInt((num1 / num2) * 1000);
+  return answer;
+}
+
+// solution(3, 2);
+// 1500
