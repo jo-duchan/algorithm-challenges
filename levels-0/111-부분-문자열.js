7a5297b4001e3fb65de59518237731c99140b54f
jo-duchan <jo_duchan@icloud.com>
2024-10-08 20:33:27 +0900

    부분 문자열

---
+++ levels-0/111-부분-문자열.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181842
+
+function solution(str1, str2) {
+  return str2.includes(str1) ? 1 : 0;
+}
+
+// solution("abc", "aabcc");
+// 1
