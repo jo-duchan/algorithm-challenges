3ffa65e22e4ba89065af6e07502c9be6432aa6ef
jo-duchan <jo_duchan@icloud.com>
2024-10-14 21:48:33 +0900

    배열 뒤집기

---
+++ levels-0/115-배열-뒤집기.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120821
+
+function solution(num_list) {
+  const answer = num_list.reverse();
+  return answer;
+}
+
+// solution([1, 2, 3, 4, 5]);
+// [5, 4, 3, 2, 1]
