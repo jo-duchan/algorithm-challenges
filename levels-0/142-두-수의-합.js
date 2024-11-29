6236025ff1c3481dcb1a41f6bc436b327ae3a4e8
jo-duchan <jo_duchan@icloud.com>
2024-11-30 00:12:04 +0900

    두 수의 합

---
+++ levels-0/142-두-수의-합.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181846
+
+function solution(a, b) {
+  return String(BigInt(a) + BigInt(b));
+}
+
+// solution("18446744073709551615", "287346502836570928366");
+// 305793246910280479981
