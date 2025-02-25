098d1369b3c632cd431dd5e40e223fd9a743e543
Duchan <jo_duchan@icloud.com>
2025-02-25 22:41:09 +0900

    카운트 다운

---
+++ recap/levels-0/020-카운트-다운.js
@@ -0,0 +1,14 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181899
+
+function solution(start_num, end_num) {
+  var answer = [];
+
+  for (let i = start_num; i >= end_num; i--) {
+    answer.push(count);
+  }
+
+  return answer;
+}
+
+// solution(10, 3);
+// [10, 9, 8, 7, 6, 5, 4, 3]
