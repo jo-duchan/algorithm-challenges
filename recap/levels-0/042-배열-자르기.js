9b9a6f1b366f518228061b55ab3646c806b1207e
Duchan <jo_duchan@icloud.com>
2025-04-25 23:31:32 +0900

    배열 자르기

---
+++ recap/levels-0/042-배열-자르기.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120833
+
+function solution(numbers, num1, num2) {
+  return numbers.slice(num1, num2 + 1);
+}
+
+// solution([1, 2, 3, 4, 5], 1, 3);
+// [2, 3, 4]
