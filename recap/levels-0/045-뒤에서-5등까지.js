3483cbd749c3034f7cd1caa6d63cee285b6a3a52
Duchan <jo_duchan@icloud.com>
2025-05-02 22:53:40 +0900

    뒤에서 5등까지

---
+++ recap/levels-0/045-뒤에서-5등까지.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181853
+
+function solution(num_list) {
+  return num_list.sort((a, b) => a - b).slice(0, 5);
+}
+
+// solution([12, 4, 15, 46, 38, 1, 14]);
+// [1, 4, 12, 14, 15]
