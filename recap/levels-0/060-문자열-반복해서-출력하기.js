0b67a1f336c1ba280078d8ce2e831cdf3454d55d
Duchan <jo_duchan@icloud.com>
2025-06-02 23:40:07 +0900

    문자열 반복해서 출력하기

---
+++ recap/levels-0/060-문자열-반복해서-출력하기.js
@@ -0,0 +1,8 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181950
+
+function solution(str, n) {
+  return str.repeat(n);
+}
+
+// solution(string, 5);
+// stringstringstringstringstring
