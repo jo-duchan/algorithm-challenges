24a86363a74906543ce3f96f8488598286a00db7
Duchan <jo_duchan@icloud.com>
2025-06-04 23:26:58 +0900

    대문자로 바꾸기

---
+++ recap/levels-0/061-대문자로-바꾸기.js
@@ -0,0 +1,9 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/181877
+
+function solution(myString) {
+  const answer = myString.toLocaleUpperCase();
+  return answer;
+}
+
+// solution('aBcDeFg');
+// "ABCDEFG"
